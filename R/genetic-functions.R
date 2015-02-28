213b0c3a5f939fbbb7968ac8603d7f1103b3b3e4
Luke Browne <lukembrowne@gmail.com>
2015-02-27 21:04:50 -0600

    Add gene diversity and allele frequency calculations

--- R/genetic-functions.R
+++ R/genetic-functions.R
@@ -17,16 +17,20 @@ makeLociNames <- function(n_loci){
 }
 
 
-# Small function that takes genotypes of mom and dad and creates a new genotype
+# Function that takes genotypes of mom and dad and creates a new genotype
 # for offspring
-breed <- function(data, params, counter){
-# Issue here that it can't assign genotypes to offsprings of adults that recruited into the population - need to make the function recursive?
-  
+chooseGenotypesForOffspring <- function(data, params, counter){
+
+      # Find plants that don't have genotypes yet
     where_na <- which(apply(data[1:counter$plant, params$loci_names], 1, anyNA))
+    
+    if(length(where_na) == 0){
+      cat("All plants already have genotypes... exiting chooseGenotypes function")
+      return(data)
+    }
   
     offspring_ids <- data$id[where_na]
     
-  
       # Subset out ids of mothers and fathers
     id_mother <- data$id_mother[offspring_ids]
     id_father <- data$id_father[offspring_ids]
@@ -45,7 +49,7 @@ breed <- function(data, params, counter){
   
     # If there are any plants left that don't have a genotype, breed again!
   if(any(apply(data[1:counter$plant, params$loci_names], 1, anyNA))){
-    data <- breed(data, params, counter)
+    data <- chooseGenotypesForOffspring(data, params, counter)
   }
   
   return(data)
@@ -57,5 +61,43 @@ chooseAllele <- function(params){
   seq(from = 1, to = params$n_loci * 2, by = 2) +
     sample(c(0,1), params$n_loci/2, replace = TRUE)
 }
+
+## Calculate allele frequencies
+# If diploid, give two lists of alleles
+calcAlleleFreq <- function(alleles_1, alleles_2 = NULL){
+  
+    # Get rid of NAs in case they are there
+  alleles <- c(alleles_1, alleles_2)
+  alleles <- na.omit(alleles)
+  
+    # Sum number of instances per allele and divide by total length
+  freq <- as.numeric(table(alleles) / length(alleles))
+  return(freq)
+}
+
+## Calculate unbiased gene diversity for one locus
+## Formula from Nei 1987 that works well with both haploid and diploid data
+calcHe <- function(alleles_1, alleles_2 = NULL){
+    
+    # Get rid of NAs in case they are there
+  alleles <- c(alleles_1, alleles_2)
+  alleles <- na.omit(alleles)
+  
+    # Used in correcting for bias in small sample sizes
+    # Will set ploidy_multiplier to 2 if diploid genotypes are passed to function
+  ploidy_multiplier <- ifelse(length(alleles_2 > 0), 2, 1)
+  
+    # Calc number of samples and allele frequencie
+  n_samples <- length(alleles_1)
+  freq <- calcAlleleFreq(alleles)
+  
+    # Formula for unbiased gene diversity from Nei 1987
+  he <- ((ploidy_multiplier * n_samples) / (ploidy_multiplier * n_samples - 1)) *
+    (1 - sum(freq^2))
+  
+  return(he)
+}
+
+
 
 
