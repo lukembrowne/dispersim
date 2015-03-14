f5da252f400cfec6f09985764bd1d82a07054114
Luke Browne <lukembrowne@gmail.com>
2015-03-13 22:26:59 -0500

    Rework plotKernels to not require GGplot2

--- R/plotting-functions.R
+++ R/plotting-functions.R
@@ -34,16 +34,20 @@ plotSummary <- function(sim){
   
     # Set colors
   adult_col <- "grey4"
-  seedling_col <- "red"
+  seedling_col <- "aquamarine3"
   
   par(mfrow = c(2,3), mar = c(3.1, 4, 2, 1))
     # Plot adult population size
   plot(sim$summary$generation, sim$summary$n_adults_alive, type = "l", lwd = 2,
        las = 1, main = "Adults", ylab = "N", xlab = "", col = adult_col,
        ylim = c(0, max(sim$summary$n_adults_alive)))
+  
     # Plot seedling population size
   plot(sim$summary$generation, sim$summary$n_seedlings_alive, type = "l", lwd = 2,
        las = 1, main = "Seedlings", ylab = "", xlab = "Steps", col = seedling_col)
+  
+  # Plot landscape
+  plotSim(sim)
   
     # Plot He
   plot(sim$summary$generation, sim$summary$he_adults_alive, type = "l", lwd = 2,
@@ -51,6 +55,13 @@ plotSummary <- function(sim){
        ylim = c(0, 1), col = adult_col)
   lines(sim$summary$generation, sim$summary$he_seedlings_alive, type = "l", lwd = 2,
         col = seedling_col)
+  
+    # Plot Nae
+  plot(sim$summary$generation, sim$summary$nae_adults, type = "l", lwd = 2,
+            las = 1, ylim = c(0, sim$params$n_alleles_per_loci), 
+            ylab = "Nae", xlab = "", main = "Nae", col = adult_col)
+    lines(sim$summary$generation, sim$summary$nae_seedlings, type = "l", lwd = 2,
+             col = seedling_col)
   
     # Plot Sp over time
   plot(sim$summary$generation, sim$summary$sp_adults, type = "l", lwd = 2,
@@ -60,8 +71,6 @@ plotSummary <- function(sim){
         col = seedling_col)  
   
   
-    # Plot landscape
-  plotSim(sim)
     # Reset par settings
   on.exit(par(old_par))
 }
@@ -80,13 +89,58 @@ plotKernels <- function(sim){
   
   dat$pollen_kernel <- rweibull(dim(dat)[1], shape = sim$params$pollen_kernel_shape,
                                 scale = sim$params$pollen_kernel_scale)
+
+    # Save kernel density estimates
+    seed_obs <- density(dat$seed_obs)
+    seed_kernel <- density(dat$seed_kernel)
+    pollen_obs <- density(dat$pollen_obs)
+    pollen_eff_obs <- density(dat$pollen_eff_obs)
+    pollen_kernel <- density(dat$pollen_kernel)
+    
+    # Find max y values for both pollen and seed for plotting
+    max_seed_y <- max(seed_obs$y, seed_kernel$y)
+    max_pollen_y <- max(pollen_obs$y, pollen_eff_obs$y, pollen_kernel$y)
+    
+    
+    # Set colors
+    seed_obs_col <- "green"
+    seed_kernel_col <- "purple"
+    pollen_obs_col <- "purple"
+    pollen_eff_obs_col <- "green"
+    pollen_kernel_col <- "red"
+    alpha_value <- .25
+    
+    # Set par settings
+    old_par <- par("mfrow", "mar")
+    par(mfrow = c(2,1), mar = c(3.1, 4, 2, 1))
   
-  dat_melt <- reshape2::melt(dat)
+    # Seed dispersal kernel 
+  plot(seed_obs, xlab = "Distance (m)", las = 1, main = "Seed dispersal",
+       ylim = c(0, max_seed_y * 1.1))
+  polygon(seed_obs, col = scales::alpha(seed_obs_col, alpha_value))
+    
+  lines(seed_kernel)
+  polygon(seed_kernel, col = scales::alpha(seed_kernel_col, alpha_value))
+    
+  legend("topright", c("Observed", "Kernel"), 
+         fill = c(scales::alpha(seed_obs_col, alpha_value),                                                       scales::alpha(seed_kernel_col, alpha_value)))
   
-  ggplot2::ggplot(dat_melt, ggplot2::aes(x = value, fill = variable)) +
-    ggplot2::geom_density(alpha = .2) +
-    ggplot2::labs(x = "Distance", y = "") + 
-    ggplot2::theme(legend.title=  ggplot2::element_blank())
+    # Pollen dispersal kernel
+  plot(pollen_obs, xlab = "Distance (m)", las = 1, main = "Pollen dispersal",
+       ylim = c(0, max_pollen_y * 1.1))
+  polygon(pollen_obs, col = scales::alpha(pollen_obs_col, alpha_value))
+    
+  lines(pollen_eff_obs)
+  polygon(pollen_eff_obs, col = scales::alpha(pollen_eff_obs_col, alpha_value))
+    
+  lines(pollen_kernel)
+  polygon(pollen_kernel, col = scales::alpha(pollen_kernel_col, alpha_value))
+  legend("topright", c("Observed", "Effective", "Kernel"), 
+            fill = c(scales::alpha(pollen_obs_col, alpha_value), 
+                     scales::alpha(pollen_eff_obs_col, alpha_value),
+                     scales::alpha(pollen_kernel_col, alpha_value)))
+  
+  on.exit(par(old_par))
 }
 
 
